d5a71cefa34d81586983c1b5c2a7b26ab9a466e4
Mara Schulke <mara@schulke.xyz>
2021-08-23 17:05:02 +0200

    Add an examples folder

---
+++ examples/counter.rs
@@ -0,0 +1,27 @@
+use async_std::task::spawn;
+use async_sub::Observable;
+
+#[async_std::main]
+async fn main() {
+    let mut observable = Observable::new(0);
+    let mut tasks = vec![];
+
+    for i in 0..10 {
+        let mut subscription = observable.subscribe();
+
+        tasks.push(spawn(async move {
+            let update = subscription.wait().await;
+
+            println!(
+                "Task {} was notified about updated observable {}",
+                i, update
+            );
+        }));
+    }
+
+    observable.publish(1);
+
+    for t in tasks {
+        t.await
+    }
+}
